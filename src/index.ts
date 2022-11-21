f7b3aff7b827b81bc4bb5abbb14494496d1a9597
swensson <lxswensson@gmail.com>
2022-11-21 08:49:55 +0100

    Cleanup and make everything work together

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import Bootstrap from './bootstrap'
-import GameOfLife from './game-of-life';
+import Bootstrap from './core/bootstrap'
+import Navigator from './core/navigator';
+import GameOfLife from './core/game-of-life';
+import AdvancedEvents from './core/advanced-events';
+import ApplicationModes from './core/application-modes';
 
-import AdvancedEvents from './advanced-events';
+import ReactToolbar from './ui/react-toolbar';
 
-import ReactToolbar from './react-toolbar';
-
-import ApplicationModes from './application-modes';
-import BrowsingMode from './browsing-mode';
-import DrawingMode from './drawing-mode';
-import ExecutionMode from './execution-mode';
-import SelectionMode from './selection-mode';
+import BrowsingMode from './modes/browsing-mode';
+import DrawingMode from './modes/drawing-mode';
+import ExecutionMode from './modes/execution-mode';
+import SelectionMode from './modes/selection-mode';
 
 document.addEventListener('DOMContentLoaded', (e) => {
 	const root = document.getElementById('app');
 
 	const bootstrap = new Bootstrap(root);
-	const gameOfLife = new GameOfLife(bootstrap);
-
+	const navigator = new Navigator(bootstrap);
+	const gameOfLife = new GameOfLife(bootstrap, navigator);
 	const events = new AdvancedEvents(bootstrap);
+	const modes = new ApplicationModes();
 
 	const toolbar = new ReactToolbar(bootstrap);
 
-	const modes = new ApplicationModes();
-	const browsing = new BrowsingMode(toolbar, events, modes, gameOfLife);
-	const drawing = new DrawingMode(modes, toolbar, events, gameOfLife, browsing);
-	const selection = new SelectionMode(toolbar, modes, events, gameOfLife);
+	const browsing = new BrowsingMode(toolbar, events, modes, gameOfLife, navigator);
+	const drawing = new DrawingMode(modes, toolbar, events, gameOfLife, navigator);
+	const selection = new SelectionMode(toolbar, modes, events, gameOfLife, navigator);
 	const execution = new ExecutionMode(toolbar, gameOfLife, modes, events);
 });
